1d7919fb28528aa483609535d49f670d938815e9
Rajesh Saini <saini222302@gmail.com>
2022-11-29 22:35:25 -0800

    clone

--- src/components/store/IndexStore.jsx
+++ src/components/store/IndexStore.jsx
@@ -11,7 +11,7 @@ const reducerCounter = (state = DEFAULT_STATE, action) => {
     return { ...state, counter: state.counter + 1 };
   }
   if (action.type == "decrement") {
-    return { ...state, counter: state.counter - 1 };
+    return { ...state, counter: state.counter - 4 };
   }
   return state;
 };
